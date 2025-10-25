4e8cd71c25a14cccc1b3a06fd268568807bd42de
Rupesh <rupeshghimire377@gmail.com>
2025-10-25 21:32:46 +0545

    Indexing

---
+++ multidimentional.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array = np.array([[['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I', 'J', 'K', 'L']],
+                [['M', 'N', 'O', 'P'], ['Q', 'R', 'S', 'T'], ['U', 'V', 'W', 'X']],
+                [['Y', 'Z', '1', '2'], ['3', '4', '5', '6'], ['7', '8', '9', '0']]])
+
+print(array.shape)
+print(array[1][2][2]) #chain indexing
+
+yo = array[1,1,1] + array[1,2,0] + array[1,0,3] + array[0,1,0] + array[1, 1, 2] + array[0,1,3] #preferred indexing
+print(yo)
+
