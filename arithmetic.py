bd1e7df727a102800899f68ee41d1391cc81cced
Rupesh <rupeshghimire377@gmail.com>
2025-10-26 14:32:03 +0545

    arithmetic done

---
+++ arithmetic.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# Scalar Arithmetic
+
+# radii = np.array([1, 2, 3])
+#
+# print(array + 1)
+# print(array * 2)
+#
+#
+# # vectorized Math function
+#
+# print(np.sqrt(array))
+# print(np.round(array))
+# print(np.pi)
+
+#
+# excercise
+
+# print(np.pi * radii ** 2)
+
+
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(array1 + array2)
+print(array1 - array2)
+print(array1 * array2)
+print(array1 ** array2)
+
+
+# comparision operators
+
+scores = np.array([23, 32, 54, 65, 76])
+
+
+scores[scores < 60] = 0
+print(scores)
