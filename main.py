afe7df1db9976e63aa15e835f8d21be7e13bca2d
Rupesh <rupeshghimire377@gmail.com>
2025-10-26 14:20:38 +0545

    slicing done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import numpy as np
 
-print(np.__version__)
+# print(np.__version__)  (to check the version of the numpy library)
+
+# my_list = [1, 2, 3, 4]
+#
+#
+# my_list = my_list * 2
+# print("Original list:", my_list)
+
+array = np.array([1, 2, 3, 4])
+array = array * 2
+print(array)
+print(type(array))
