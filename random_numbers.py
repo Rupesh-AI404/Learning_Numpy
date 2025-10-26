fd47a5a3d793eaf31af06fdc15acb1d709aca987
Rupesh <rupeshghimire377@gmail.com>
2025-10-26 21:35:49 +0545

    final commit for learning

---
+++ random_numbers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# rng = np.random.default_rng(seed = 1)  #seed is the random number generator
+#
+# print(rng.integers(1, 100, size = 3))  #generate 10 random integers between 1 and 100 with the size of 3
+#
+#
+# print(np.random.uniform(1.0, 5.0, size = (2, 3)))  #generate random float numbers between 1.0 and 5.0 with the size of 2x3 array
+
+
+rng = np.random.default_rng()
+
+fruits = np.array(['apple', 'banana', 'cherry', 'coconut', 'date', 'elderberry', 'fig'])
+fruits = rng.choice(fruits, size = (2,2), replace = False)
+
+print(fruits)
