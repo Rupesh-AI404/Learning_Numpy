f463bbef53f2c4ef30094b94d850e02874bc3b96
Rupesh <rupeshghimire377@gmail.com>
2025-10-27 23:44:47 +0545

    final commit for learning

--- random_numbers.py
+++ random_numbers.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-# rng = np.random.default_rng(seed = 1)  #seed is the random number generator
-#
-# print(rng.integers(1, 100, size = 3))  #generate 10 random integers between 1 and 100 with the size of 3
-#
-#
-# print(np.random.uniform(1.0, 5.0, size = (2, 3)))  #generate random float numbers between 1.0 and 5.0 with the size of 2x3 array
+rng = np.random.default_rng(seed = 1)  #seed is the random number generator
+
+print(rng.integers(1, 100, size = 3))  #generate 10 random integers between 1 and 100 with the size of 3
+
+
+print(np.random.uniform(1.0, 5.0, size = (2, 3)))  #generate random float numbers between 1.0 and 5.0 with the size of 2x3 array
 
 
 rng = np.random.default_rng()
