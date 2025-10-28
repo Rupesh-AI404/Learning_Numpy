027cfea1e9c599d0ab1e8a6f9c163304621adef1
Rupesh <rupeshghimire377@gmail.com>
2025-10-28 23:15:29 +0545

    final commit for learning

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 
 # print(np.__version__)  (to check the version of the numpy library)
 
-# my_list = [1, 2, 3, 4]
-#
-#
-# my_list = my_list * 2
-# print("Original list:", my_list)
+my_list = [1, 2, 3, 4]
+
+
+my_list = my_list * 2
+print("Original list:", my_list)
 
 array = np.array([1, 2, 3, 4])
 array = array * 2
