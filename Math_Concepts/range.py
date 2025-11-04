e62d4aa20f05208a24b4d9c10e29f00f0fa6be44
Rupesh <rupeshghimire377@gmail.com>
2025-11-04 23:44:43 +0545

    Range

---
+++ Math_Concepts/range.py
@@ -0,0 +1,8 @@
+data =  [1200, 1500, 3400, 4444]
+minimum = min(data)
+maximum = max(data)
+range_value = maximum - minimum
+
+print(f"Minimum: {minimum}")
+print(f"Maximum: {maximum}")
+print(f"Range: {range_value}")
