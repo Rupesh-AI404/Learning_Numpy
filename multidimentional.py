41bd4cf6960c78d9d4197e317798c1947fe93f26
Rupesh <rupeshghimire377@gmail.com>
2025-12-04 21:29:27 +0545

    Math Concepts

--- multidimentional.py
+++ multidimentional.py
@@ -10,6 +10,12 @@ print(array[1][2][2]) #chain indexing
 yo = array[1,1,1] + array[1,2,0] + array[1,0,3] + array[0,1,0] + array[1, 1, 2] + array[0,1,3] #preferred indexing
 print(yo)
 
+one_dimensional_arr = np.array([10, 12])
+print(one_dimensional_arr)
+
+two_dimensional_arr = np.array([[10, 12], [13, 14]])
+print(two_dimensional_arr)
+
 
 
 
