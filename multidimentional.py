5795fd011d977b66dbd6f4d19e87ac9090b69c97
Rupesh <rupeshghimire377@gmail.com>
2025-12-06 21:28:53 +0545

    Math Concepts

--- multidimentional.py
+++ multidimentional.py
@@ -5,7 +5,8 @@ array = np.array([[['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I', 'J', 'K', '
                 [['Y', 'Z', '1', '2'], ['3', '4', '5', '6'], ['7', '8', '9', '0']]])
 
 print(array.shape)
-print(array[1][2][2]) #chain indexing
+print(array[1][2][2])
+#chain indexing
 
 yo = array[1,1,1] + array[1,2,0] + array[1,0,3] + array[0,1,0] + array[1, 1, 2] + array[0,1,3] #preferred indexing
 print(yo)
