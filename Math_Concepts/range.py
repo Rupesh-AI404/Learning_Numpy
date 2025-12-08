6edb9f109380e464f33de76e23035a699bc72939
Rupesh <rupeshghimire377@gmail.com>
2025-12-08 20:46:14 +0545

    Math Concepts

--- Math_Concepts/range.py
+++ Math_Concepts/range.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 data =  [1200, 1500, 3400, 4444]
 minimum = min(data)
 maximum = max(data)
@@ -6,5 +8,12 @@ range_value = maximum - minimum
 print(f"Minimum: {minimum}")
 print(f"Maximum: {maximum}")
 print(f"Range: {range_value}")
+print(f"Mean: {(minimum + maximum) / 2}")
+print(f"Median: {data[2]}")
+print(f"Mode: {max(set(data), key=data.count)}")
+print(f"Standard Deviation: {round(np.std(data), 2)}")
+print(f"Variance: {round(np.var(data), 2)}")
+print(f"Sum: {sum(data)}")
+print(f"Product: {np.prod(data)}")
 
 
